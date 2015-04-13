71802c11b7458e9f249d1cbbbab95c04cdaf5a19
Ken Crocken <kcrocken@gmail.com>
2015-04-12 22:08:35 -0400

    modifies schema

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -17,7 +17,7 @@ module Jekyll
       source = "<div class=\"video\">"
 
       source += "<div class=\"video-wrapper\">"
-      source += "<iframe width=\"640\" height=\"480\" src=\"//www.youtube.com/embed/#{@url}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>"
+      source += "<iframe itemprop="video" width=\"640\" height=\"480\" src=\"//www.youtube.com/embed/#{@url}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>"
       source += "</div>"
       source += "</div>"
 
